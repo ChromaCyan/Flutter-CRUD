056d300fd1ef610a1a45a7008b0ccab0904feb3a
Josh Brian Bugarin <jofe.bugarin.up@phinmaed.com>
2024-09-08 19:11:28 +0800

    Setup preparation for API implementation

---
+++ lib/widgets/studentcard.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Studentcard extends StatelessWidget {
+  const Studentcard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
